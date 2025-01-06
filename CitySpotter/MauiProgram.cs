424a40ef66f8bbeeea4333c0f7d949971ff122ee
Zehond <fsas.7seal@gmail.com>
2025-01-06 10:47:24 +0100

    added maps logic

--- CitySpotter/MauiProgram.cs
+++ CitySpotter/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CitySpotter.Domain.Services;
+using Microsoft.Extensions.Logging;
 
 namespace CitySpotter
 {
@@ -9,14 +10,21 @@ namespace CitySpotter
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiMaps()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+
+            builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+
+            builder.Services.AddSingleton<MapViewModel>();
+            builder.Services.AddSingleton<MapPage>(s => new MapPage(s.GetRequiredService<MapViewModel>()));
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
