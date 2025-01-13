a8028666fce6f4e7b52fccb2cb0730583c6ee7a3
Erik <er.dijkstra@student.avans.nl>
2025-01-14 00:40:30 +0100

    Settingss work

--- CitySpotter/Domain.Services/SettingsViewModel.cs
+++ CitySpotter/Domain.Services/SettingsViewModel.cs
@@ -7,6 +7,7 @@ using CommunityToolkit.Mvvm.Messaging;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
@@ -19,15 +20,23 @@ namespace CitySpotter.Domain.Services
     public partial class SettingsViewModel : ObservableObject
     {
         [ObservableProperty]
-        private string name = LocalizationResources.Instance["name"].ToString();
-
-        [ObservableProperty]
-        private string pageTheme = "Resources/Styles/NormalMode.xaml";
-        //private ResourceDictionary pageTheme = new Resources.Styles.NormalMode();
+        private string pickerItem;
 
         public LocalizationResources LocalizationResources => LocalizationResources.Instance;
 
         public bool isis = true;
+
+        public SettingsViewModel()
+        {
+            Debug.WriteLine(CultureInfo.CurrentCulture);
+            if (CultureInfo.CurrentCulture.Equals(new CultureInfo("nl-NL")))
+            {
+                PickerItem = "Nederlands";
+            } else
+            {
+                PickerItem = "English";
+            }
+        }
 
         [RelayCommand]
         private void Button_Clicked()
@@ -42,42 +51,18 @@ namespace CitySpotter.Domain.Services
                 WeakReferenceMessenger.Default.Send(new ThemeChangedMessage("NormalMode"));
             }
             isis = !isis;
-            //Preferences.Set("theme", "ColorBlindMode");
-            //WeakReferenceMessenger.Default.Send(new ThemeChangedMessage("ColorBlindMode"));
-            //LoadTheme("ColorBlindMode");
         }
 
         [RelayCommand]
-        private void NederlandsButton_Clicked()
+        private void LanguageChange()
         {
-            Debug.WriteLine("Nederlands");
-            LocalizationResources.SetCulture(new CultureInfo("nl-NL"));
-            Name = LocalizationResources["name"].ToString();
-        }
-
-        [RelayCommand]
-        private void EnglishButton_Clicked()
-        {
-            Debug.WriteLine("English");
-            LocalizationResources.SetCulture(new CultureInfo("en-US"));
-            Name = LocalizationResources["name"].ToString();
-        }
-
-        private void LoadTheme(string theme)
-        {
-            ResourceDictionary dict = theme switch
+            Debug.WriteLine(PickerItem);
+            if (pickerItem.Equals("Nederlands"))
             {
-                "ColorBlindMode" => new Resources.Styles.ColorBlindMode(),
-                "NormalMode" => new Resources.Styles.NormalMode()
-            };
-
-            if (dict != null)
+                LocalizationResources.SetCulture(new CultureInfo("nl-NL"));
+            } else
             {
-                //Resources.MergedDictionaries.Clear();
-                //Resources.MergedDictionaries.Add(dict);
-                //PageTheme = dict;
-                
-                PageTheme = "Resources/Styles/ColorBlindMode.xaml";
+                LocalizationResources.SetCulture(new CultureInfo("en-US"));
             }
         }
     }
